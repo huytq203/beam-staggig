5c98928ff63b27187d6a0678c8d41dd93272c5a0
huytq203 <huytq203@gmail.com>
2026-07-20 14:26:59 +0700

    feat: update password validation requirements to minimum 10 characters in Auth and createAccount schemas

--- validations/Auth.schema.ts
+++ validations/Auth.schema.ts
@@ -4,10 +4,10 @@ export const ResetPasswordSchema = yup.object({
   password: yup
     .string()
     .required('Vui lòng nhập mật khẩu.')
-    .min(8, 'Mật khẩu phải chứ ít nhất 8 ký tự.')
+    .min(10, 'Mật khẩu phải chứ ít nhất 10 ký tự.')
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      'Mật khẩu phải gồm ít nhất 8 ký tự trong đó phải bao gồm cả chữ viết thường, chữ viết hoa, chữ cái số và ký tự đặc biệt'
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{10,})/,
+      'Mật khẩu phải gồm ít nhất 10 ký tự trong đó phải bao gồm cả chữ viết thường, chữ viết hoa, chữ cái số và ký tự đặc biệt'
     )
     .typeError('Vui lòng nhập mật khẩu.'),
   confirmPassword: yup
@@ -26,10 +26,10 @@ export const ChangePasswordSchema = yup.object({
   newPassword: yup
     .string()
     .required('Vui lòng nhập mật khẩu.')
-    .min(8, 'Mật khẩu phải chứ ít nhất 8 ký tự.')
+    .min(8, 'Mật khẩu phải chứ ít nhất 10 ký tự.')
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      'Mật khẩu phải gồm ít nhất 8 ký tự trong đó phải bao gồm cả chữ viết thường, chữ viết hoa, chữ cái số và ký tự đặc biệt'
+      'Mật khẩu phải gồm ít nhất 10 ký tự trong đó phải bao gồm cả chữ viết thường, chữ viết hoa, chữ cái số và ký tự đặc biệt'
     )
     .typeError('Vui lòng nhập mật khẩu.'),
   confirmPassword: yup
