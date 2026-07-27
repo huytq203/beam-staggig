bf385e0317adadb4aca2525fc5e70833e215d43b
huytq203 <huytq203@gmail.com>
2026-07-27 15:56:39 +0700

    feat: add payLimitPerEmployee column to ListAccountsCompany table

--- src/modules/companies/accounts/ListAccountsCompany.tsx
+++ src/modules/companies/accounts/ListAccountsCompany.tsx
@@ -276,6 +276,13 @@ const ListAccountsCompany = (props: any) => {
       render: (e: any) => <p>{StringHelper.formatVND(e, '-')}</p>,
     },
     {
+      title:'Hạn mức tối đa mỗi người lao động',
+      dataIndex: 'payLimitPerEmployee',
+       align: 'right',
+      width: 250,
+      render: (e: any) => <p>{StringHelper.formatVND(e, '-')}</p>,
+    },
+    {
       title: `Hạn mức tính theo ngày công thực tế`,
       dataIndex: 'payLimitByWorkDay',
       width: 250,
