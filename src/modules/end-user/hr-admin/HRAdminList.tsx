4b9d3de6395dc9061ff3f891e03455bbdc61657a
huytq203 <huytq203@gmail.com>
2026-08-04 14:11:23 +0700

    update

--- src/modules/end-user/hr-admin/HRAdminList.tsx
+++ src/modules/end-user/hr-admin/HRAdminList.tsx
@@ -1,7 +1,7 @@
 import { AppPagination } from '@components/shared';
 import AppTable from '@components/shared/AppTable/AppTable';
 import { IconEdit,IconLock } from '@douyinfe/semi-icons';
-import { Tag, Typography } from '@douyinfe/semi-ui';
+import { Tag, Tooltip, Typography } from '@douyinfe/semi-ui';
 import { StringHelper } from '@helpers/string.helper';
 import { UserSevice } from '@services/users';
 import { useRouter } from 'next/router';
@@ -72,15 +72,21 @@ export const HRAdminList = (props: any) => {
                 <span className="beam-break-world">{username}</span>
               </Text>
             )}
-             {record.accountLocked ? (
-              <Tag
-                className="ml-2 align-middle"
-                color="orange"
-                prefixIcon={<IconLock size="small" />}
-                size="small"
+            {record.accountLocked ? (
+              <Tooltip
+                content="Tài khoản đã bị tạm khóa do đăng nhập sai 5 lần."
+                position="top"
               >
-                Đã khóa
-              </Tag>
+                <Tag
+                  className="ml-2 align-middle"
+                  color="orange"
+                  prefixIcon={<IconLock size="small" />}
+                  size="small"
+                  tabIndex={0}
+                >
+                  Đã khóa
+                </Tag>
+              </Tooltip>
             ) : null}
           </>
         );
